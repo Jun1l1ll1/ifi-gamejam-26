2988bfd9c35f54cdf48ca976adce87db727fb586
Jun1l1ll1 <juni@grottenveien.no>
2026-02-07 13:00:35 +0100

    Rotate player when moveing

--- src/Player.py
+++ src/Player.py
@@ -11,7 +11,8 @@ class Player:
         self.y = 640
         self.size = (70, 70)
         self.color = BLUE
-        self.image = pygame.transform.scale(PLAYER2_IMAGE, self.size)
+        self._base_image = pygame.transform.scale(PLAYER2_IMAGE, self.size)
+        self.image = self._base_image
         self.speed = PLAYER_SPEED
         self.last_shot_time = 0
 
@@ -25,8 +26,26 @@ class Player:
         screen.blit(self.image, (self.x, self.y))
     
     def move(self, v: list[int], dt):
-        length = m.sqrt(v[0]**2 + v[1]**2)
+        length = self._normalize(v)
         if length <= 0: return
+
+        self.image = pygame.transform.rotate(self._base_image, self._move_angle(v))
 
         self.x += self.speed * (v[0]/length) * dt
         self.y += self.speed * (v[1]/length) * dt
+
+    def _normalize(self, v):
+        return m.sqrt(v[0]**2 + v[1]**2)
+
+    def _move_angle(self, v):
+        u = [1, 0] # Sprite is rotated right by default
+        dot_product = sum(i*j for i, j in zip(u, v))
+        norm_u = self._normalize(u)
+        norm_v = self._normalize(v)
+        cos_theta = dot_product / (norm_u * norm_v)
+        angle_rad = m.acos(cos_theta)
+
+        dir = 1 # Make angle other way when going down
+        if v[1] == 1: dir = -1
+
+        return m.degrees(angle_rad) * dir
