893180febfb1679fb9015cf2d802c32cf031a670
Jun1l1ll1 <juni@grottenveien.no>
2026-02-08 12:57:53 +0100

    Fixed rotation of enemies

--- src/entities/InvadingAlien.py
+++ src/entities/InvadingAlien.py
@@ -48,24 +48,24 @@ class InvadingAlien(pygame.sprite.Sprite):
         dx += random.uniform(-0.1, 0.1)
         dy += random.uniform(-0.1, 0.1)
 
-        length = self._normalize(v)
+        length = self._length(v)
         if length <= 0: return
 
         self.image = pygame.transform.rotate(self._base_image, self._move_angle(v))
 
-    def _normalize(self, v):
+    def _length(self, v):
        return math.sqrt(v[0]**2 + v[1]**2)
 
     def _move_angle(self, v):
-        u = [0, -1] # Sprite is rotated right by default
+        u = [1, 0] # Sprite is rotated right by default
         dot_product = sum(i*j for i, j in zip(u, v))
-        norm_u = self._normalize(u)
-        norm_v = self._normalize(v)
+        norm_u = self._length(u)
+        norm_v = self._length(v)
         cos_theta = dot_product / (norm_u * norm_v)
         angle_rad = math.acos(cos_theta)
 
         dir = 1 # Make angle other way when going down
-        if v[1] == -1: dir = -1
+        if v[1] > 0: dir = -1
 
         return math.degrees(angle_rad) * dir
 
